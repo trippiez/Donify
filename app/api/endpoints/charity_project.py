71ebfb31dbae390d331a71d9653e0fb13d116b29
Tom Bulmer <tombulmer@MacBook-Pro.local>
2024-05-11 08:18:30 +0300

    add validate endpoints

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.validators import (check_charity_project_exists,
-                                check_name_duplicate)
+                                check_name_duplicate, check_full_amount_greater_than_invested, check_project_not_fully_invested, check_charity_project_invested_exists)
 from app.core.db import get_async_session
 from app.crud.charity_project import charity_project_crud
 from app.models import Donation
@@ -10,6 +10,7 @@ from app.schemas.charity_project import (CharityProjectCreate,
                                          CharityProjectDB,
                                          CharityProjectUpdate)
 from app.services.invest import invest
+from app.core.user import current_superuser
 
 router = APIRouter()
 
@@ -29,7 +30,8 @@ async def get_all_charity_projects(
 @router.post(
     '/',
     response_model=CharityProjectDB,
-    response_model_exclude_none=True
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
 )
 async def create_new_charity_project(
     charity_project: CharityProjectCreate,
@@ -43,7 +45,8 @@ async def create_new_charity_project(
 @router.patch(
     '/{charity_project_id}',
     response_model=CharityProjectDB,
-    response_model_exclude_none=True
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
 )
 async def partially_update_charity_project(
     charity_project_id: int,
@@ -51,9 +54,15 @@ async def partially_update_charity_project(
     session: AsyncSession = Depends(get_async_session)
 ):
     charity_project = await check_charity_project_exists(charity_project_id, session)
+    await check_project_not_fully_invested(charity_project)
 
     if obj_in.name is not None:
         await check_name_duplicate(obj_in.name, session)
+
+    if obj_in.full_amount is not None:
+        await check_full_amount_greater_than_invested(
+            charity_project, obj_in.full_amount
+        )
 
     charity_project = await charity_project_crud.update(
         charity_project, obj_in, session
@@ -65,12 +74,15 @@ async def partially_update_charity_project(
 @router.delete(
     '/{charity_project_id}',
     response_model=CharityProjectDB,
-    response_model_exclude_none=True
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
 )
 async def remove_charity_project(
     charity_project_id: int,
     session: AsyncSession = Depends(get_async_session)
 ):
     project = await check_charity_project_exists(charity_project_id, session)
+    await check_charity_project_invested_exists(project)
+
     project = await charity_project_crud.remove(project, session)
     return project
--- app/api/validators.py
+++ app/api/validators.py
@@ -16,7 +16,7 @@ async def check_name_duplicate(
 
     if project_id is not None:
         raise HTTPException(
-            status_code=422,
+            status_code=400,
             detail='Благотворительный проект с таким именем уже существует!'
         )
 
@@ -36,3 +36,34 @@ async def check_charity_project_exists(
             detail='Благотворительный проект не найден!'
         )
     return project
+
+
+async def check_full_amount_greater_than_invested(
+    charity_project: CharityProject,
+    obj_full_amount: int
+):
+    if charity_project.invested_amount > obj_full_amount:
+        raise HTTPException(
+            status_code=400,
+            detail='Сумма пожертвования не может быть меньше внесённой!'
+        )
+
+
+async def check_project_not_fully_invested(
+    charity_project: CharityProject
+):
+    if charity_project.fully_invested == 1:
+        raise HTTPException(
+            status_code=400,
+            detail='Благотворительный проект полностью проинвестирован!'
+        )
+
+
+async def check_charity_project_invested_exists(
+    charity_project: CharityProject
+):
+    if charity_project.invested_amount > 0:
+        raise HTTPException(
+            status_code=400,
+            detail='Средства в благотворительный проект уже были внесены!'
+        )
